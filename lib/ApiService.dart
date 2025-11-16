875ca0ebe53ca0a8bf28b5f77a53e80415739824
Abdul-Rahim-Flutter <abdulrahimyosufi@gmail.com>
2025-11-16 22:52:25 +0430

    Caching feature has been added

--- lib/ApiService.dart
+++ lib/ApiService.dart
@@ -1,10 +1,9 @@
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:restful_api_practical/ErrorResponse.dart';
 
 class ApiService {
-  static const String baseUrl = 'https://gorest.co.in/public/v2/posts';
-
   // //get request to fetch posts
   // Future<List<dynamic>> fetchPosts() async {
   //   try{
@@ -19,27 +18,54 @@ class ApiService {
   //     await ErrorHandler.handle(e, null);
   //     return [];
   //   }
-    
+
   // }
+  static const String baseUrl = 'https://gorest.co.in/public/v2/posts';
+
+  //creating of a cache manager instance
+  final CacheManager _cacheManager = DefaultCacheManager();
 
   //get request to fetch posts
   Future<List<dynamic>> fetchPosts(int page, int limit) async {
-    try{
-      final response = await http.get(Uri.parse('$baseUrl?page=$page&limit=$limit'));
-    if (response.statusCode == 200) {
-      print('Decoded Posts: ${response.body}');
-      return jsonDecode(response.body);
-    } else {
-      await ErrorHandler.handle(null, response);
-      return [];
-    }
-    }catch(e){
+    try {
+      //try to load the response from cache
+      final fileInfo = await _cacheManager.getFileFromCache(
+        '$baseUrl?page=$page&limit=$limit',
+      );
+
+      //checks whether the cached file is available and valid
+      if (fileInfo != null && fileInfo.validTill.isAfter(DateTime.now())) {
+        //read the saved file as raw bytes
+        final cachedData = await fileInfo.file.readAsBytes();
+        print("Cached data $cachedData");
+
+        //convert the raw bytes to readable JSON format
+        final decode = jsonDecode(utf8.decode(cachedData));
+        print('Decoded Cached Posts: $decode');
+        return decode;
+      }
+      final response = await http.get(
+        Uri.parse('$baseUrl?page=$page&limit=$limit'),
+      );
+      if (response.statusCode == 200) {
+        //store the response in cache for future use
+        await _cacheManager.putFile(
+          '$baseUrl?page=$page&limit=$limit',
+          response.bodyBytes,
+          maxAge: Duration(seconds: 50),
+        );
+        
+        print('Decoded Posts: ${response.body}');
+        return jsonDecode(response.body);
+      } else {
+        await ErrorHandler.handle(null, response);
+        return [];
+      }
+    } catch (e) {
       await ErrorHandler.handle(e, null);
       return [];
     }
-    
   }
-
 
   //post request to create a post
   Future<void> createPost(String title, String body) async {
